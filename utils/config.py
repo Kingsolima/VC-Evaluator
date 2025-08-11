9a2fea32e9217aaff80c5f9b881830fb5c5bd3cb
Omar Soliman <omr.m.soliman@gmail.com>
2025-08-11 15:51:28 -0400

    updating token.json and credentials.json

--- utils/config.py
+++ utils/config.py
@@ -10,3 +10,18 @@ GMAIL_SENDER        = os.getenv('GMAIL_SENDER', '')
 
 SPREADSHEET_ID      = os.getenv('SPREADSHEET_ID', '')
 SHEET_RANGE         = os.getenv('SHEET_RANGE', 'Sheet1!A1')
+
+import json, tempfile
+
+GOOGLE_TOKEN_JSON = os.getenv("GOOGLE_TOKEN_JSON", "")
+if GOOGLE_TOKEN_JSON:
+    try:
+        info = json.loads(GOOGLE_TOKEN_JSON)
+        tmp = tempfile.NamedTemporaryFile(prefix="google_token_", suffix=".json", delete=False)
+        tmp.write(json.dumps(info).encode("utf-8"))
+        tmp.flush()
+        # this is what email.py & sheet.py use
+        GOOGLE_TOKEN_PATH = tmp.name
+    except Exception:
+        pass
+
